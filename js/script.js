7b273a4ad697b01b9eac37114b6629ff7cfd805e
lucasjsanti <lucas_swatoficial@hotmail.com>
2022-12-20 11:02:58 -0300

    Alteração font-size do slide

--- js/script.js
+++ js/script.js
@@ -23,3 +23,22 @@ Fancybox.bind('[data-fancybox="gallery"]', {
 Fancybox.fromOpener('[data-fancybox="gallery"]', {
   startIndex: 1,
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function passaSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.querySelector(".mySlideVideo");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
